402629ae8f097900a43e0f2f56ae02f6bccf9fac
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-06-10 21:34:02 +0100

    Added More functionality

--- vars/withSpringBootPipeline.groovy
+++ vars/withSpringBootPipeline.groovy
@@ -211,13 +211,13 @@ def call(String type, String tenant, String component, Closure body) {
                 }
                 steps
                 {
-                    archiveReports {}
+                    withArchiveReports {}
                 }
             }
 
             stage("Archive HTML Reports artifacts")
             {
-                archiveHtmlReports {}
+                withArchiveHtmlReports {}
             }
 
         }
