cc7fd5b59f75fdc186ab047fa72efce816743fb7
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-06-10 20:42:39 +0100

    Switch statement

--- vars/withWorldpayContinuosIntegrationPipeline.groovy
+++ vars/withWorldpayContinuosIntegrationPipeline.groovy
@@ -1,8 +1,12 @@
 def call(String type, String tenant, String component, Closure body) {
 
     switch (type) {
-        case "java" -> withSpringBootPipeline(type, tenant, component) {}
-        default -> error "ERROR: Unsupported pipeline type used '${type}'"
+        case "java":
+            withSpringBootPipeline(type, tenant, component) {}
+            break
+        default:
+            error "ERROR: Unsupported pipeline type used '${type}'"
+            break
     }
     body.call()
 }
