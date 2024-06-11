dc77a9cf488f849ce4bd0106d58b0d59b1c451f1
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-06-11 20:17:43 +0100

    fix unused variable

--- vars/gradleBuildOnly.groovy
+++ vars/gradleBuildOnly.groovy
@@ -3,7 +3,14 @@
  */
 
 def call() {
-	sh """
+
+    def profiles = ""
+
+    if (params.release) {
+        profiles += "-Prelease"
+    }
+
+    sh """
             ./gradlew ${env.SERVICE_NAME}:clean ${env.SERVICE_NAME}:build ${profiles}
-        """
+    """
 }
