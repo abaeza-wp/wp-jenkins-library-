9102c64e00636af6c190ba03cedc2f90911c00d2
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-06-11 15:48:21 +0100

    remove unsupported mask password

--- vars/withBuildImage.groovy
+++ vars/withBuildImage.groovy
@@ -5,8 +5,7 @@
 */
 
 def call(Closure body) {
-    script
-    {
+    script {
 
         // Read deployment profile
         def profile = readYaml(file: "deployment/profiles/${params.profile}.yml")
@@ -30,22 +29,19 @@ def call(Closure body) {
 */
 
 def executeImageBuild(profile, kubernetesToken, versionName) {
-    maskPasswords(varPasswordPairs: [[var: 'kubernetesToken']]) {
+    def profiles = ""
 
-        def profiles = ""
+    if (params.release) {
+        profiles += "-Prelease"
+    }
 
-        if (params.release) {
-            profiles += "-Prelease"
-        }
-
-        /**
-         * env.GIT_COMMIT_TIMESTAMP
-         This is needed to set the container creation date to the last commit date.
-         without doing this, Jib sets the creation time to Unix epoch (00:00:00, January 1st, 1970 in UTC) and
-         might breaks the behaviour when the file mod timestamp gets used for caching purpose.
-         For more info see https://github.com/GoogleContainerTools/jib/blob/master/docs/faq.md#why-is-my-image-created-48-years-ago
-         */
-        sh """
+    /**
+     * env.GIT_COMMIT_TIMESTAMP
+     This is needed to set the container creation date to the last commit date.
+     without doing this, Jib sets the creation time to Unix epoch (00:00:00, January 1st, 1970 in UTC) and
+     might breaks the behaviour when the file mod timestamp gets used for caching purpose.
+     For more info see https://github.com/GoogleContainerTools/jib/blob/master/docs/faq.md#why-is-my-image-created-48-years-ago*/
+    sh """
             ./gradlew ${env.SERVICE_NAME}:clean ${env.SERVICE_NAME}:build ${env.SERVICE_NAME}:jib \
                 -Djib.to.image=${profile.build.docker_registry}/${profile.deploy.namespace}/${env.SERVICE_NAME}:${versionName} \
                 -Djib.to.auth.username=${profile.deploy.ocp_username} \
@@ -54,11 +50,8 @@ def executeImageBuild(profile, kubernetesToken, versionName) {
                 -Djib.container.filesModificationTime=${env.GIT_COMMIT_TIMESTAMP} \
                 ${profiles}
         """
-    }
 }
 
 def isCreateNamespace(profile) {
-    return profile.deploy.create_namespace != null &&
-    profile.deploy.create_namespace.enabled != null &&
-    profile.deploy.create_namespace.enabled
+    return profile.deploy.create_namespace != null && profile.deploy.create_namespace.enabled != null && profile.deploy.create_namespace.enabled
 }
