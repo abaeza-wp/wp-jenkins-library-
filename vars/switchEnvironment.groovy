f314ad4f2b68349a31b6cca09ebc4a30bf4af337
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-06-11 22:54:44 +0100

    Fix echo

--- vars/switchEnvironment.groovy
+++ vars/switchEnvironment.groovy
@@ -3,7 +3,8 @@ import com.worldpay.pipeline.BuildConfigurationMapper
 def call(String environment) {
     var currentBuildConfig = BuildConfigurationMapper.getClusterInformationForAwsRegion(environment, "${params.awsRegion}")
     echo """
-			Switched to:
+    Successfully switched environment context to:
+
 			Environment: ${currentBuildConfig.cluster.environment}
 			AWS Region: ${currentBuildConfig.cluster.awsRegion}
 			Profile Name: ${currentBuildConfig.profileName}
