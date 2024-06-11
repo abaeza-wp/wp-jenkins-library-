aef9ec8fb9b7ba0faa34ac8b935a4452b4585f72
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-06-11 21:05:30 +0100

    Attempt to override stage to include callbacks

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,7 +1,7 @@
 import com.worldpay.PipelineRunner
 
 // Override Stage built-in step to include pipeline callbacks
-def call(STAGE_NAME, Closure body) {
+def call(String STAGE_NAME, Closure body) {
 
     PipelineRunner.getRunner().runWithCallbacks(STAGE_NAME) {
         def res = steps.stage(STAGE_NAME) {
