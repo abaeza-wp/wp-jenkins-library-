9a832962fc27a0553625c122d83da0f25a024001
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-07-15 21:52:07 +0100

    Added image promotion

---
+++ vars/withImagePromotionDynamicStage.groovy
@@ -0,0 +1,59 @@
+import com.worldpay.context.BuildContext
+import com.worldpay.utils.TokenHelper
+
+def call(String sourceEnvironment, String destinationEnvironment, String clusterUsername, String sourceCredentialId, String sourceNamespace) {
+    def environmentName = BuildContext.currentBuildProfile.cluster.environment
+    def awsRegion = BuildContext.currentBuildProfile.cluster.awsRegion
+
+    if (BuildContext.useFunctionalEnvironments) {
+        for (functionalEnvironment in BuildContext.functionalEnvironments) {
+            stage("[${environmentName}] [${functionalEnvironment}] Promote Image") {
+                def destinationCredentialId = TokenHelper.tokenNameOf(environmentName, BuildContext.componentName, awsRegion, functionalEnvironment)
+
+                def sourceProfile = BuildContext.getBuildProfileForAwsRegion(sourceEnvironment, awsRegion)
+                def destinationProfile = BuildContext.getBuildProfileForAwsRegion(destinationEnvironment, awsRegion)
+
+                def destinationNamespace = "${env.NAMESPACE}-${functionalEnvironment}"
+
+                def sourceRegistry = sourceProfile.cluster.imageRegistry
+                def destinationRegistry = destinationProfile.cluster.imageRegistry
+
+                //Obtain tokens
+                def sourceRegistryToken = kubernetesLogin(sourceProfile.cluster.api, clusterUsername, sourceCredentialId, sourceNamespace, false)
+                def destinationRegistryToken = kubernetesLogin(destinationProfile.cluster.api, null, destinationCredentialId, destinationNamespace, false)
+
+                promoteImageFromTo(
+                sourceNamespace,
+                sourceRegistryToken,
+                sourceRegistry,
+                destinationNamespace,
+                destinationRegistryToken,
+                destinationRegistry)
+            }
+        }
+    } else {
+        stage("[${environmentName}] Promote Image") {
+
+            def destinationCredentialId = TokenHelper.tokenNameOf(environmentName, BuildContext.componentName, awsRegion)
+
+            def sourceProfile = BuildContext.getBuildProfileForAwsRegion(sourceEnvironment, awsRegion)
+            def destinationProfile = BuildContext.getBuildProfileForAwsRegion(destinationEnvironment, awsRegion)
+
+            def destinationNamespace = "${env.NAMESPACE}"
+            def sourceRegistry = sourceProfile.cluster.imageRegistry
+            def destinationRegistry = destinationProfile.cluster.imageRegistry
+
+            //Obtain tokens
+            def sourceRegistryToken = kubernetesLogin(sourceProfile.cluster.api, clusterUsername, sourceCredentialId, sourceNamespace, false)
+            def destinationRegistryToken = kubernetesLogin(destinationProfile.cluster.api, null, destinationCredentialId, destinationNamespace, false)
+
+            promoteImageFromTo(
+            sourceNamespace,
+            sourceRegistryToken,
+            sourceRegistry,
+            destinationNamespace,
+            destinationRegistryToken,
+            destinationRegistry)
+        }
+    }
+}
