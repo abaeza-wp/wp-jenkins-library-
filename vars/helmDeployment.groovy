edfc18142b1e539c5e625d3e0e92d48834b4c079
e5697436 <153538325+abaeza-wp@users.noreply.github.com>
2024-07-22 16:10:41 +0100

    NOTICKET: Only use functional environment values.yaml when configured to do so

--- vars/helmDeployment.groovy
+++ vars/helmDeployment.groovy
@@ -49,10 +49,10 @@ def call(String functionalEnvironment, namespace, String token) {
     archiveArtifacts artifacts: "${releasePackageFileName}"
 
     def options = [
-        "--set global.awsRegion=${awsRegion}",
-        "--set global.environment=${environment}",
-        "--set global.clusterName=${clusterName}",
-        "--set global.imageTag=${appVersion}",
+    "--set global.awsRegion=${awsRegion}",
+    "--set global.environment=${environment}",
+    "--set global.clusterName=${clusterName}",
+    "--set global.imageTag=${appVersion}",
     ]
 
     if (functionalEnvironment != null) {
@@ -79,10 +79,10 @@ def call(String functionalEnvironment, namespace, String token) {
     options.add("--namespace=${namespace}") //Just for extra safety
 
     def optionsString = (options + [
-        "--history-max 3",
-        "--install",
-        "--wait",
-        "--timeout 180s"
+    "--history-max 3",
+    "--install",
+    "--wait",
+    "--timeout 180s"
     ]).join(' ')
 
     def valuesFilesString = getAllValuesFilesIfExist(chartLocation, environment, functionalEnvironment, awsRegion)
@@ -96,15 +96,10 @@ def call(String functionalEnvironment, namespace, String token) {
 
 String getAllValuesFilesIfExist(String chartLocation, String environment, String functionalEnvironment, String awsRegion) {
 
-    def valuesFilesFound = [
-        "${chartLocation}/values.yaml"] //This is the default values file.
+    def valuesFilesFound = ["${chartLocation}/values.yaml"] //This is the default values file.
     //Support for environment specific values.<env>.yaml e.g values.dev.yaml, values.stage.yaml, values.prod.yaml
     if (fileExists("${chartLocation}/values.${environment}.yaml")) {
         valuesFilesFound.add("${chartLocation}/values.${environment}.yaml")
-    }
-    //Support for environment specific values.<functionalEnvironment>.yaml e.g values.live.yaml, values.try.yaml
-    if (fileExists("${chartLocation}/values.${functionalEnvironment}.yaml")) {
-        valuesFilesFound.add("${chartLocation}/values.${functionalEnvironment}.yaml")
     }
     //Support for region specific values.<region>.yaml e.g values.eu-west-1.yaml, values.us-east-1.yaml
     if (fileExists("${chartLocation}/values.${awsRegion}.yaml")) {
@@ -114,13 +109,19 @@ String getAllValuesFilesIfExist(String chartLocation, String environment, String
     if (fileExists("${chartLocation}/values.${environment}.${awsRegion}.yaml")) {
         valuesFilesFound.add("${chartLocation}/values.${environment}.${awsRegion}.yaml")
     }
-    //Support for environment and region specific values.<functionalEnvironment>.<region>.yaml e.g values.live.eu-west-1.yaml, values.try.us-east-1.yaml
-    if (fileExists("${chartLocation}/values.${functionalEnvironment}.${awsRegion}.yaml")) {
-        valuesFilesFound.add("${chartLocation}/values.${functionalEnvironment}.${awsRegion}.yaml")
-    }
-    //Support for environment and region specific values.<env>.<functionalEnvironment>.<region>.yaml e.g values.dev.live.eu-west-1.yaml, values.dev.try.us-east-1.yaml
-    if (fileExists("${chartLocation}/values.${environment}.${functionalEnvironment}.${awsRegion}.yaml")) {
-        valuesFilesFound.add("${chartLocation}/values.${environment}.${functionalEnvironment}.${awsRegion}.yaml")
+    if (BuildContext.useFunctionalEnvironments) {
+        //Support for environment specific values.<functionalEnvironment>.yaml e.g values.live.yaml, values.try.yaml
+        if (fileExists("${chartLocation}/values.${functionalEnvironment}.yaml")) {
+            valuesFilesFound.add("${chartLocation}/values.${functionalEnvironment}.yaml")
+        }
+        //Support for environment and region specific values.<functionalEnvironment>.<region>.yaml e.g values.live.eu-west-1.yaml, values.try.us-east-1.yaml
+        if (fileExists("${chartLocation}/values.${functionalEnvironment}.${awsRegion}.yaml")) {
+            valuesFilesFound.add("${chartLocation}/values.${functionalEnvironment}.${awsRegion}.yaml")
+        }
+        //Support for environment and region specific values.<env>.<functionalEnvironment>.<region>.yaml e.g values.dev.live.eu-west-1.yaml, values.dev.try.us-east-1.yaml
+        if (fileExists("${chartLocation}/values.${environment}.${functionalEnvironment}.${awsRegion}.yaml")) {
+            valuesFilesFound.add("${chartLocation}/values.${environment}.${functionalEnvironment}.${awsRegion}.yaml")
+        }
     }
     return valuesFilesFound.flatten().join(' -f ')
 }
